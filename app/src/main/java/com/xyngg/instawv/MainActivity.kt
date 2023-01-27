d71228544c4f4f822b5016b423a4aaf3ec4a0c6e
lepras <heyabhinav@protonmail.com>
2023-01-27 05:53:33 -0500

    proxy draft

--- app/src/main/java/com/xyngg/instawv/MainActivity.kt
+++ app/src/main/java/com/xyngg/instawv/MainActivity.kt
@@ -8,13 +8,20 @@ import android.view.View
 import android.webkit.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Proxy
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
-    private var mapsWebView: WebView? = null
-    private var mapsWebSettings: WebSettings? = null
+    private var instaWebView: WebView? = null
+    private var instaWebSettings: WebSettings? = null
     private var mapsCookieManager: CookieManager? = null
 
     private val allowedDomains = ArrayList<String>()
@@ -42,12 +49,12 @@ class MainActivity : AppCompatActivity() {
         //}
 
         //Create the WebView
-        mapsWebView = findViewById<View>(R.id.mapsWebView) as WebView
+        instaWebView = findViewById<View>(R.id.InstaWebView) as WebView
 
         //Set cookie options
         var mapsCookieManager = CookieManager.getInstance()
         mapsCookieManager.setAcceptCookie(true)
-        mapsCookieManager.setAcceptThirdPartyCookies(mapsWebView, false)
+        mapsCookieManager.setAcceptThirdPartyCookies(instaWebView, false)
 
         //Delete anything from previous sessions
 //        resetWebView(false)
@@ -57,7 +64,15 @@ class MainActivity : AppCompatActivity() {
 
         //Restrict what gets loaded
         initURLs()
-        mapsWebView!!.webViewClient = object : WebViewClient() {
+        instaWebView!!.webViewClient = object : WebViewClient() {
+            var hostname = "localhost" /*127.0.0.1*/
+            var port = 1080
+            var proxy: Proxy = Proxy(
+                Proxy.Type.SOCKS,
+                InetSocketAddress(hostname, port)
+            )
+            private val okHttp: OkHttpClient = OkHttpClient.Builder().proxy(proxy).build()
+
             //Keep these in sync!
             override fun shouldInterceptRequest(
                 view: WebView,
@@ -124,6 +139,14 @@ class MainActivity : AppCompatActivity() {
                         ) //Deny URLs on DENYLIST
                     }
                 }
+                val okHttpRequest: Request = Request.Builder().url(request.url.toString()).build()
+                try {
+                    val response: Response = okHttp.newCall(okHttpRequest).execute()
+                    return WebResourceResponse("", "", response.body!!.byteStream())
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
                 return null
             }
 
@@ -183,24 +206,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         //Set more options
-        var mapsWebSettings = mapsWebView!!.settings
+        var instaWebSettings = instaWebView!!.settings
         //Enable some WebView features
-        mapsWebSettings.javaScriptEnabled = true
-        mapsWebSettings.cacheMode = WebSettings.LOAD_DEFAULT
-        mapsWebSettings.setGeolocationEnabled(true)
+        instaWebSettings.javaScriptEnabled = true
+        instaWebSettings.cacheMode = WebSettings.LOAD_DEFAULT
+        instaWebSettings.setGeolocationEnabled(true)
         //Disable some WebView features
-        mapsWebSettings.allowContentAccess = false
-        mapsWebSettings.allowFileAccess = false
-        mapsWebSettings.builtInZoomControls = false
-        mapsWebSettings.databaseEnabled = false
-        mapsWebSettings.displayZoomControls = false
-        mapsWebSettings.domStorageEnabled = false
-        mapsWebSettings.saveFormData = false
+        instaWebSettings.allowContentAccess = false
+        instaWebSettings.allowFileAccess = false
+        instaWebSettings.builtInZoomControls = false
+        instaWebSettings.databaseEnabled = false
+        instaWebSettings.displayZoomControls = false
+        instaWebSettings.domStorageEnabled = false
+        instaWebSettings.saveFormData = false
         //Change the User-Agent
-        mapsWebSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36")
+        instaWebSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 12; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36")
 
         //Load Instagram
-        mapsWebView!!.loadUrl(urlToLoad)
+        instaWebView!!.loadUrl(urlToLoad)
     }
 
     override fun onDestroy() {
@@ -213,8 +236,8 @@ class MainActivity : AppCompatActivity() {
         if (event.action == KeyEvent.ACTION_DOWN) {
             when (keyCode) {
                 KeyEvent.KEYCODE_BACK -> {
-                    if (mapsWebView!!.canGoBack() && mapsWebView!!.url != "about:blank") {
-                        mapsWebView!!.goBack()
+                    if (instaWebView!!.canGoBack() && instaWebView!!.url != "about:blank") {
+                        instaWebView!!.goBack()
                     } else {
                         finish()
                     }
@@ -227,24 +250,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun resetWebView(exit: Boolean) {
         if (exit) {
-            mapsWebView!!.loadUrl("about:blank")
-            mapsWebView!!.removeAllViews()
-            mapsWebSettings!!.javaScriptEnabled = false
+            instaWebView!!.loadUrl("about:blank")
+            instaWebView!!.removeAllViews()
+            instaWebSettings!!.javaScriptEnabled = false
         }
-        //mapsWebView.clearCache(true);
-        mapsWebView!!.clearFormData()
-        mapsWebView!!.clearHistory()
-        mapsWebView!!.clearMatches()
-        mapsWebView!!.clearSslPreferences()
+        //instaWebView.clearCache(true);
+        instaWebView!!.clearFormData()
+        instaWebView!!.clearHistory()
+        instaWebView!!.clearMatches()
+        instaWebView!!.clearSslPreferences()
 //        mapsCookieManager!!.removeSessionCookie()
 //        mapsCookieManager!!.removeAllCookie()
         CookieManager.getInstance().removeAllCookies(null)
         CookieManager.getInstance().flush()
         WebStorage.getInstance().deleteAllData()
         if (exit) {
-            mapsWebView!!.destroyDrawingCache()
-            mapsWebView!!.destroy()
-            mapsWebView = null
+            instaWebView!!.destroyDrawingCache()
+            instaWebView!!.destroy()
+            instaWebView = null
         }
     }
 
